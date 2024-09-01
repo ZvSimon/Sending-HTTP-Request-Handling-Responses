0313919a6e9041ca2a70e94eea972c4c9e88fe65
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-01 20:23:57 +0200

    Sending Get request to Fetch Data and display it on the page

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,30 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {Place} from '../place.model';
+import {PlacesComponent} from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {HttpClient} from "@angular/common/http";
+import {map, pipe} from "rxjs";
 
 @Component({
-  selector: 'app-available-places',
-  standalone: true,
-  templateUrl: './available-places.component.html',
-  styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+    selector: 'app-available-places',
+    standalone: true,
+    templateUrl: './available-places.component.html',
+    styleUrl: './available-places.component.css',
+    imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
-  places = signal<Place[] | undefined>(undefined);
+export class AvailablePlacesComponent implements OnInit {
+    places = signal<Place[] | undefined>(undefined);
+    private httpClient = inject(HttpClient);
+
+    ngOnInit() {
+        const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
+            .pipe(map(responseData =>
+                responseData.places)).subscribe({
+                next: (places) => {
+                    this.places.set(places);
+                },
+            });
+
+    }
 }
